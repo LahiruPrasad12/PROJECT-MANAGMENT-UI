52d1b973ef0a4f5195d276d6926aa86999f35b08
amilasm <amilasampath1400@gmail.com>
2022-04-05 08:44:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import { Container,Button,Modal } from 'semantic-ui-react'
+import LandingPage from "./components/Pages/Landing Page/landingPage"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import React from "react"
+
 function App() {
   return (
-    <Container className='mt-1'>
-    <p>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-      ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-      ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-      quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-      arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-      Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-      dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-      Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-      Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-      viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-    </p>
-    <Button primary>Primary</Button>
-    <Button secondary>Secondary</Button>
-    <Modal
-      trigger={<Button>Show Modal</Button>}
-      header='Reminder!'
-      content='Call Benjamin regarding the reports.'
-      actions={['Snooze', { key: 'done', content: 'Done', positive: true }]}
-    />
-  </Container>
-  );
+    <Router>
+      <div>
+        <LandingPage/>
+      </div>
+      
+    </Router>
+  )
 }
 
 export default App;
