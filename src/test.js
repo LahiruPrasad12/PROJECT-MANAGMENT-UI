174d93b135fe241ff2e6664b37a29670171307f9
LahiruPrasad12 <lahirupr471@gmail.com>
2022-05-29 17:57:37 +0530

    created route for get topics categories

--- src/test.js
+++ src/test.js
@@ -1,112 +1,83 @@
 import * as React from 'react';
-import LoadingButton from '@mui/lab/LoadingButton';
+import {Theme, useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import SaveIcon from '@mui/icons-material/Save';
-import SendIcon from '@mui/icons-material/Send';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+import {string} from "yup";
+import {useState} from "react";
 
-export default function LoadingButtonsTransition() {
-    const [loading, setLoading] = React.useState(true);
-    function handleClick() {
-        setLoading(true);
-    }
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+];
+
+
+export default function MultipleSelectChip() {
+    const theme = useTheme();
+    const [personName, setPersonName] = useState([]);
+
+    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+        const {
+            target: {value},
+        } = event;
+        setPersonName(
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
 
     return (
-        <Box>
-            <FormControlLabel
-                sx={{
-                    display: 'block',
-                }}
-                control={
-                    <Switch
-                        checked={loading}
-                        onChange={() => setLoading(!loading)}
-                        name="loading"
-                        color="primary"
-                    />
-                }
-                label="Loading"
-            />
-            <Box sx={{ '& > button': { m: 1 } }}>
-                <LoadingButton
-                    size="small"
-                    onClick={handleClick}
-                    loading={loading}
-                    variant="outlined"
-                    disabled
+        <div>
+            <FormControl sx={{m: 1, width: 300}}>
+                <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
+                <Select
+                    labelId="demo-multiple-chip-label"
+                    id="demo-multiple-chip"
+                    value={personName}
+                    onChange={handleChange}
+                    input={<OutlinedInput id="select-multiple-chip" label="Chip"/>}
+                    renderValue={(selected) => (
+                        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                            {selected.map((value) => (
+                                <Chip key={value} label={value}/>
+                            ))}
+                        </Box>
+                    )}
+                    MenuProps={MenuProps}
                 >
-                    disabled
-                </LoadingButton>
-                <LoadingButton
-                    size="small"
-                    onClick={handleClick}
-                    loading={loading}
-                    loadingIndicator="Loading..."
-                    variant="outlined"
-                >
-                    Fetch data
-                </LoadingButton>
-                <LoadingButton
-                    size="small"
-                    onClick={handleClick}
-                    endIcon={<SendIcon />}
-                    loading={loading}
-                    loadingPosition="end"
-                    variant="contained"
-                >
-                    Send
-                </LoadingButton>
-                <LoadingButton
-                    size="small"
-                    color="secondary"
-                    onClick={handleClick}
-                    loading={loading}
-                    loadingPosition="start"
-                    startIcon={<SaveIcon />}
-                    variant="contained"
-                >
-                    Save
-                </LoadingButton>
-            </Box>
-
-            <Box sx={{ '& > button': { m: 1 } }}>
-                <LoadingButton
-                    onClick={handleClick}
-                    loading={loading}
-                    variant="outlined"
-                    disabled
-                >
-                    disabled
-                </LoadingButton>
-                <LoadingButton
-                    onClick={handleClick}
-                    loading={loading}
-                    loadingIndicator="Loading..."
-                    variant="outlined"
-                >
-                    Fetch data
-                </LoadingButton>
-                <LoadingButton
-                    onClick={handleClick}
-                    endIcon={<SendIcon />}
-                    loading={loading}
-                    loadingPosition="end"
-                    variant="contained"
-                >
-                    Send
-                </LoadingButton>
-                <LoadingButton
-                    color="secondary"
-                    onClick={handleClick}
-                    loading={loading}
-                    loadingPosition="start"
-                    startIcon={<SaveIcon />}
-                    variant="contained"
-                >
-                    Save
-                </LoadingButton>
-            </Box>
-        </Box>
+                    {names.map((name) => (
+                        <MenuItem
+                            key={name}
+                            value={name}
+                            // style={getStyles(name, personName, theme)}
+                        >
+                            {name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </div>
     );
 }
