e027b8bbb3f52c2c135e28c1827ef1b10d3a2193
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-05-29 22:15:07 +0530

    setup spervisor files

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import AdminRoutes from './routes/adminRouters';
-import AuthRoutes from './routes/authRoutes'
-import StudentRoutes from './routes/studentRoutes'
+import React from "react";
+import AdminRoutes from "./routes/adminRouters";
+import AuthRoutes from "./routes/authRoutes";
+import StudentRoutes from "./routes/studentRoutes";
+import SupervisorRoutes from "./routes/supervisorRoutes";
 
 const Routers = () => {
-    return (
-       <div>
-           <AuthRoutes/>
-           <AdminRoutes/>
-           <StudentRoutes/>
-       </div>
-    );
+  return (
+    <div>
+      <AuthRoutes />
+      <AdminRoutes />
+      <StudentRoutes />
+      <SupervisorRoutes />
+    </div>
+  );
 };
 
 export default Routers;
---
+++ src/views/supervisor/supervisorHome.js
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Sidenav from "../../layouts/sidenavsupervisor";
+import "./studenthome.css";
+
+export default function SupervisorHome() {
+  return <></>;
+}
