bd4b845699e50298b969146fcf2ed2753b757e3c
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-05-31 21:42:04 +0530

    Hide buttons in navigation bar in mobile view

--- src/views/dashboard/supervisor-dashboard/supervisorHome.js
+++ src/views/dashboard/supervisor-dashboard/supervisorHome.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Sidenav from "../../layouts/sidenavsupervisor";
+import Sidenav from "../../../layouts/sidenavsupervisor";
 import "./supervisorHome.css";
 
 export default function SupervisorHome() {
