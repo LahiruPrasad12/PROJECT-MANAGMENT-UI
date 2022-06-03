a8e6c6eb00c0c989c03ca09493ff0b440636395a
LahiruPrasad12 <lahirupr471@gmail.com>
2022-06-04 04:50:42 +0530

    created loader for home page

--- src/views/dashboard/student-dashboard/home.jsx
+++ src/views/dashboard/student-dashboard/home.jsx
@@ -11,11 +11,19 @@ import Loader from '../../../loader/loader'
 export default function Studenthome() {
 
     const [members, setMembers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+        setLoading(true)
         const getMyGroup = async () => {
-            const respond = (await groupAPI.myGroupMember()).data.data.filteredData
-            console.log(respond)
-            setMembers(respond)
+            try {
+                const respond = (await groupAPI.myGroupMember()).data.data.filteredData
+                console.log(respond)
+                setMembers(respond)
+            } catch (e) {
+
+            }
+            setLoading(false)
         }
         getMyGroup()
     }, [])
@@ -24,86 +32,98 @@ export default function Studenthome() {
         <>
 
             <Sidenav/>
-            <Loader/>
-            <div class="content">
 
-                <div class="container">
-                    <center>
-                        <h1>GROUP DETAILS</h1>
-                        <p>Student</p>
-                        <Button className="btn btn-primary" hidden={members.length === 4} sx={{
-                            float: 'right'
-                        }}
-                            data-bs-toggle="modal" data-bs-target="#addStudent" variant="contained" disableElevation>Add
-                            members</Button>
-                        <div style={{marginTop: '5%'}} class="row">
-                            <div class="col">
-                                <table class="table">
-                                    <thead class="thead-dark">
-                                    <tr style={{textAlign: 'center'}}>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Student Name</th>
-                                        <th scope="col">Email</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody style={{textAlign: 'center'}}>
-                                    {
-                                        members.map((student, index) => {
-                                            return <tr>
-                                                <td>{index + 1}</td>
-                                                <td>{student.name}</td>
-                                                <td>{student.email}</td>
+            {
+                loading && (
+                    <Loader/>
+                )
+            }
+
+            {
+                !loading && (
+                    <div className="content">
+
+                        <div className="container">
+                            <center>
+                                <h1>GROUP DETAILS</h1>
+                                <p>Student</p>
+                                <Button className="btn btn-primary" hidden={members.length === 4} sx={{
+                                    float: 'right'
+                                }}
+                                        data-bs-toggle="modal" data-bs-target="#addStudent" variant="contained"
+                                        disableElevation>Add
+                                    members</Button>
+                                <div style={{marginTop: '5%'}} className="row">
+                                    <div className="col">
+                                        <table className="table">
+                                            <thead className="thead-dark">
+                                            <tr style={{textAlign: 'center'}}>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Student Name</th>
+                                                <th scope="col">Email</th>
                                             </tr>
-                                        })
-                                    }
+                                            </thead>
+                                            <tbody style={{textAlign: 'center'}}>
+                                            {
+                                                members.map((student, index) => {
+                                                    return <tr>
+                                                        <td>{index + 1}</td>
+                                                        <td>{student.name}</td>
+                                                        <td>{student.email}</td>
+                                                    </tr>
+                                                })
+                                            }
 
-                                    </tbody>
-                                </table>
-                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
-                                     aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                                <button type="button" class="close" data-dismiss="modal"
-                                                        aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                ...
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                        data-dismiss="modal">Close
-                                                </button>
-                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                            </tbody>
+                                        </table>
+                                        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog"
+                                             aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div className="modal-dialog" role="document">
+                                                <div className="modal-content">
+                                                    <div className="modal-header">
+                                                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                        <button type="button" className="close" data-dismiss="modal"
+                                                                aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div className="modal-body">
+                                                        ...
+                                                    </div>
+                                                    <div className="modal-footer">
+                                                        <button type="button" className="btn btn-secondary"
+                                                                data-dismiss="modal">Close
+                                                        </button>
+                                                        <button type="button" className="btn btn-primary">Save changes
+                                                        </button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
+                                        <br/>
                                     </div>
+
                                 </div>
-                                <br/>
-                            </div>
+                                {/*student add model*/}
 
+                            </center>
                         </div>
-                        {/*student add model*/}
 
-                    </center>
-                </div>
+                        <div className="modal fade" id="addStudent" tabIndex="-1"
+                             aria-labelledby="exampleModalLabel">
+                            <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                <div className="modal-content">
+                                    <div className="modal-body">
+                                        <AddGroupMember rowNumber={4 - members.length}/>
+                                    </div>
 
-                <div className="modal fade" id="addStudent" tabIndex="-1"
-                     aria-labelledby="exampleModalLabel">
-                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                        <div className="modal-content">
-                            <div className="modal-body">
-                                <AddGroupMember rowNumber={4 - members.length}/>
+                                </div>
                             </div>
-
                         </div>
+                        <Footerdashboard/>
                     </div>
-                </div>
-                <Footerdashboard/>
-            </div>
+                )
+            }
         </>
     )
 }
