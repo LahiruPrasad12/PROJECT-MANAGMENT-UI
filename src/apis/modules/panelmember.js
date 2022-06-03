54ed4f5d603a71f702f105787b9db567fa6392e3
LahiruPrasad12 <lahirupr471@gmail.com>
2022-06-04 00:54:37 +0530

    resolved errors

--- src/apis/modules/panelmember.js
+++ src/apis/modules/panelmember.js
@@ -1,1 +1,11 @@
+import api from "../axios";
+const resource = '/api/supervisors';
 
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default  {
+    getOurPanel: () => api.get(`${resource}`),
+    register: (payload) => api.post(`/signup`,payload),
+    currentUser: () => api.get(`/current-user`),
+    logout: () => api.get(`/logout`),
+}
