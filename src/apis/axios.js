bb1a47bbca4e0ee034107bd185d9415f09514305
kavindushehan <it20076566@my.sliit.lk>
2022-06-04 23:49:50 +0530

    final commit

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:'http://localhost:5000'
+    baseURL: 'https://project-managment-ui.vercel.app'
 })
 
-if(localStorage.getItem('JWT')){
+if (localStorage.getItem('JWT')) {
     instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('JWT')}`;
 }
 
