d6297744eb64813b1460ec1d747edccb13408b74
LahiruPrasad12 <lahirupr471@gmail.com>
2022-06-04 18:41:24 +0530

    resolve errors

--- src/views/dashboard/student-dashboard/TopicRegistration/models/SubmitTopicToSpuervisor.js
+++ src/views/dashboard/student-dashboard/TopicRegistration/models/SubmitTopicToSpuervisor.js
@@ -222,6 +222,7 @@ export default function RegisterTopicToSupervisor(props) {
                                                             })
                                                         }
 
+
                                                     </Select>
 
                                                 </FormControl>
